c2be7a58c7acd35de9ae6ee19e758c70b2a6c933
ben <945306058@qq.com>
2022-05-17 15:29:03 +0800

    添加请求js

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,5 +1,12 @@
 <template>
+
   <div class="login">
+    <header-diy
+      :backHidden="2"
+      :type="2"
+      class="topbar"
+      :titleName="pageName"
+    ></header-diy>
     账户
     <u--input
       placeholder="请输入内容"
@@ -14,16 +21,28 @@
       v-model="password"
       @change="change"
     ></u--input>
+    <u-button @click="submit" type="primary" text="确定"></u-button>
   </div>
 </template>
 <script>
+import headerDiy from "../component/header/header.vue";
+import request from '../../common/utils/request'
 export default {
+  components: {
+    headerDiy,
+  },
   data() {
     return {
-        username:'',
-        password:''
+        username:'admin',
+        password:'123456',
+        pageName: "登录",
     };
   },
+  methods:{
+      submit(){
+
+      }
+  }
 };
 </script>
 <style lang="scss" scoped>
